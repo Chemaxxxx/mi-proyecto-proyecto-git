16a432ee7a222f500b170ddef34511799f0ce609
Chemaxxxx <josemangt6@gmail.com>
2026-04-21 17:44:48 +0200

    Añadida clase Calculadora al proyecto

---
+++ Calculadora.java
@@ -0,0 +1,12 @@
+public class Calculadora {
+    public static void main(String[] args) {
+        int n1 = 10;
+        int n2 = 5;
+
+        System.out.println("--- Mi Calculadora Git ---");
+        System.out.println("La suma es: " + (n1 + n2));
+        System.out.println("La resta es: " + (n1 - n2));
+        System.out.println("La multiplicación es: " + (n1 * n2));
+        System.out.println("La división es: " + (n1 / n2));
+    }
+}
